a89ee5b50da2fa532445f7cf69c49479be890aeb
Tom Bergqvist <bergqvist.tom@gmail.com>
2019-05-25 13:50:12 +0200

    Fix.

--- Application/Queries/Accounts/AccountsQuery.cs
+++ Application/Queries/Accounts/AccountsQuery.cs
@@ -4,12 +4,12 @@ using System.Linq;
 
 namespace Application.Queries.Accounts
 {
-    public class CustomerAccountsQuery
+    public class AccountsQuery
     {
         public List<AccountDetailsModel> GetCustomerAccounts(IBankDbContext context, int customerId)
         {
             var customer = context.Customers.Single(c => c.CustomerId == customerId);
-            var accounts = context.Accounts.Where(a => a.Dispositions == customer.Dispositions);
+            var accounts = context.Accounts.Where(a => a.Dispositions == customer.Dispositions).ToList();
 
             List<AccountDetailsModel> listOfAccounts = new List<AccountDetailsModel>();
 
